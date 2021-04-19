58e665e48f92f30c933a35f78f14603ecf70b86b
Dylan Simon <dylan@dylex.net>
2021-04-19 17:33:27 -0400

    fix tests (remove outdated Blaze Word ones)

--- test/Main.hs
+++ test/Main.hs
@@ -15,8 +15,6 @@ import qualified Test.QuickCheck.Test as Q (isSuccess)
 import qualified Text.Blaze.Renderer.Utf8 as BM
 import qualified Web.Cookie as Cook
 
-import qualified Blaze.ByteString.Builder.Html.Utf8 as BU
-import qualified Blaze.ByteString.Builder.Html.Word as BW
 import qualified Waimwork.Blaze as DB
 import qualified Text.Blaze as TB
 import           Waimwork.Cookie (Secret(..), setSignedCookie, getSignedCookie)
@@ -26,22 +24,17 @@ assert :: Bool -> IO ()
 assert False = exitFailure
 assert True = return ()
 
-beq :: B.Builder -> B.Builder -> Q.Property
-beq = (Q.===) `on` B.toLazyByteString
-
 meq :: TB.Markup -> TB.Markup -> Q.Property
 meq = (Q.===) `on` BM.renderMarkup
 
 checkText :: String -> Q.Property
 checkText s = Q.label "blazeText" $
-  BU.fromHtmlEscapedText t `beq` BW.fromHtmlEscapedText t
-  Q..&&. DB.text t `meq` TB.text t
+  DB.text t `meq` TB.text t
   where t = T.pack s
 
 checkLazyText :: String -> Q.Property
 checkLazyText s = Q.label "blazeLazyText" $
-  BU.fromHtmlEscapedLazyText t `beq` BW.fromHtmlEscapedLazyText t
-  Q..&&. DB.lazyText t `meq` TB.lazyText t
+  DB.lazyText t `meq` TB.lazyText t
   where t = TL.pack s
 
 checkCookie :: EntropyPool -> String -> String -> String -> Q.Property
